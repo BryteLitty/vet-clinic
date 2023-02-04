1dd6bec760bf7602a42d4d8114e2f1c1ddc8da15
BryteLitty <littyofficials@gmail.com>
2023-02-04 05:16:53 +0000

    Add query to multiple tables

--- data.sql
+++ data.sql
@@ -32,3 +32,58 @@ VALUES('Blossom', '1998-10-13', 3, true, 17.0);
 
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES('Ditto', '2022-05-14', 4, true, 22.0);
+
+-- insert data into the owners table
+INSERT INTO owners(full_name, age)
+VALUES ('Sam Smith', 34);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Jennifer Orwell', 19);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Bob', 45);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Melody Pond', 77);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Dean Winchester', 14);
+
+INSERT INTO owners(full_name, age)
+VALUES ('Jodie Whittaker', 38);
+
+-- Insert Data into the Species Table
+INSERT INTO species (name) VALUES ('Pokemon');
+
+INSERT INTO species (name) VALUES ('Digimon'); 
+
+
+-- Modify the animals table to include species_id value
+UPDATE animals 
+SET species_id = 
+    CASE 
+        WHEN name like '%mon%' THEN
+            (SELECT id FROM species WHERE name = 'Digimon')
+        ELSE
+            (SELECT id FROM species WHERE name = 'Pokemon')
+    END;
+
+
+-- assign owners to each animal using owner_id
+UPDATE animals 
+SET owner_id = (
+    CASE 
+        WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+        WHEN name = 'Agumon' THEN (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+        WHEN name = 'Gabumon' THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+        WHEN name = 'Pikachu' THEN (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+        WHEN name = 'Devimon' THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+        WHEN name = 'Plantom' THEN (SELECT id FROM owners WHERE full_name = 'Bob')
+        WHEN name = 'Charmander' THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+        WHEN name = 'Squirtle' THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+        WHEN name = 'Blossom' THEN (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+        WHEN name = 'Angemon' THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+        WHEN name = 'Boarmon' THEN (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+        WHEN name = 'Ditto' THEN NULL
+    END
+);
